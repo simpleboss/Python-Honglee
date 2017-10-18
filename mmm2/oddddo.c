e12088076838854482fadcae7e66a75de07687bc
gichan lee <gichan.lee@3mindev.club>
2017-10-18 18:16:33 +0900

    add oddddo.md

--- mmm2/oddddo.c
+++ mmm2/oddddo.c
@@ -1,12 +1,4 @@
 #include <stdio.h>
 int main() {
-	int n, i, sum;
-	sum = 0;
-	scanf("%d", &n);
-
-	for (i = 0; i <= (n - 1); i++) {
-		sum = sum + (1 + 2 * i ) * ( 2 * ( n - 1 ) + 1 - 2 * i);
-	}
-	printf("%d", sum);
-	scanf("%d", &n);
+	int 
 }
