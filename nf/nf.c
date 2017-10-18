31ce84f0615dd115964a08561899cf0b660f3f41
gichan lee <gichan.lee@3mindev.club>
2017-10-18 22:56:04 +0900

    4 통과 but 7583 입력시 오류

--- nf/nf.c
+++ nf/nf.c
@@ -2,13 +2,15 @@
 int main() {
 	int i,n, nb_measure;
 	nb_measure = 0;
+	printf("test");
 	scanf("%d", &n);
-	for (i = 1; i <= 10000; i++) {
+
+	for (i = 1; i <= n; i++) {
 		if (n % i == 0) {
 			nb_measure++;
 		}
 	}
-	if (2 * nb_measure + 1 % 2 == 1) {
+	if ((2 * nb_measure + 1) % 2 == 1) {
 		printf("yes");
 	}
 	else {
