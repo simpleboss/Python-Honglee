9f0454b32239fbadf7c5f09899b6b5af54c90ae0
gichan lee <gichan.lee@3mindev.club>
2017-10-20 22:08:35 +0900

    23284에서 시간초과발생. 처음으로 직면한 시간초과문제

---
+++ tf/tf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int n, r, l, l_answer, r_answer;
+
+	scanf_s("%d", &n, 1000000);
+	//printf("scanf_s complete\n");
+	//printf("(int)pow(2.0, (double)0) = %d\n", (int)pow(2.0, 0.0));
+	for (l_answer = 1; l_answer <= n; l_answer = l_answer + 2) {
+		
+		for (r_answer = 0; r_answer <= n / 2; r_answer++) {
+			//printf("start for r_answer");
+			r = (int)pow(2.0, (double)r_answer);
+			if (n == l_answer * r) {
+				//printf("yes");
+				printf("%d %d\n", l_answer, r_answer);
+				break;
+			}
+		}
+	}
+	scanf_s("%d", &n,1000000);
+	return 0;
+}
