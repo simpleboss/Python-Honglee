1b39e4a55468a0fcce4cb3629ef60e2116271b37
gichan lee <gichan.lee@3mindev.club>
2017-10-20 22:41:56 +0900

    55 33 22 에서 오류

---
+++ fou/fou.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+	int first, second, third, temp, d1, d2;
+	scanf_s("%d %d %d", &first, &second, &third);
+
+	if (first > second) {
+		temp = first;
+		first = second;
+		second = temp;
+	}
+
+	if (second > third) {
+		temp = second;
+		second = third;
+		third = temp;
+	}
+
+	if (first > third) {
+		temp = first;
+		first = third;
+		third = temp;
+	}
+	//printf("first second third = %d %d %d \n", first, second, third);
+	d1 = second - first;
+	d2 = third - second;
+
+	if (d1 == d2) {
+		printf("%d", third + d1);
+	}
+	else if (d1 > d2) {
+		printf("%d", first + d2);
+	}
+	else {
+		printf("%d", second + d1);
+	}
+	scanf_s("%d %d %d", &first, &second, &third);
+
+	return 0;
+}
