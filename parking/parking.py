b84d484a470446ff9c12f60e0af7284af6335e80
gichan lee <gichan.lee@3mindev.club>
2017-11-05 21:27:20 +0900

    UnboundLocalError: local variable 'min' referenced before assignment

--- parking/parking.py
+++ parking/parking.py
@@ -4,24 +4,13 @@ test = input()
 def parking(number_store):
     i = 1
     location_store = []
-    sum_location = 0
     while i <= number_store:
         location = input()
         location_store.append(location)
-        sum_location = sum_location + location
         i += 1
-
-    avg_location = sum_location / number_store
-    print 'avg_location', avg_location
-    print 'location[0]', location_store[0]
-
-    i = 0
-    result = 0
-    while i < len(location_store):
-        result = result + abs(avg_location - location_store[i])
-        i += 1
-
-    print result
-    return result
+    min = min(location_store)
+    max = max(location_store)
+    print max - min
+    return max - min
 
 parking(test)
