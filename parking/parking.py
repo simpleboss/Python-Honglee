7acad30bceb64f6f985d65c1af3864e9f06ad188
gichan lee <gichan.lee@3mindev.club>
2017-11-06 13:17:14 +0900

    complete

--- parking/parking.py
+++ parking/parking.py
@@ -1,6 +1,6 @@
 def parking():
     test = input()
-    location_store = [int(i) for i in input().split( )]
+    location_store = [int(i) for i in raw_input().split(" ")]
     min_location = min(location_store)
     max_location = max(location_store)
     print 2 * (max_location - min_location)
