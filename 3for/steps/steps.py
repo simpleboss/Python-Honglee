5be0ab2a1258d4a778a5a4b95a8ef948058b327b
gichan lee <gichan.lee@3mindev.club>
2017-11-07 20:36:26 +0900

    complete but dirty

--- 3for/steps/steps.py
+++ 3for/steps/steps.py
@@ -7,10 +7,15 @@ def step(x, y):
             result = x * 2
         else:
             result = x * 2 + 1
-    elif x % 2 == 0 and y % 2 == 0:
-        result = x + y
-    elif x % 2 != 0 and y % 2 != 0:
-        result = x + y - 1
+    else:
+        if abs(x - y) > 2:
+            result = -1
+        elif y - x >= 2:
+            result = -1
+        elif x % 2 == 0 and y % 2 == 0:
+            result = x + y
+        elif x % 2 != 0 and y % 2 != 0:
+            result = x + y - 1
 
     if result == -1:
         print 'No Number'
