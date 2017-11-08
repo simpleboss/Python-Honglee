a678761b9b6479cd1a8196ce0b2dc57b899cc725
gichan lee <gichan.lee@3mindev.club>
2017-11-08 14:09:20 +0900

    sytle error, need to remove space on both side of '/'

---
+++ 3for/count/count.py
@@ -0,0 +1,25 @@
+def count(num):
+    total = 0
+    i = 1
+    #find *th diagonal
+    while total < num:
+        total = total + i
+        i += 1
+    #print 'total =', total
+    i = i - 1
+    #print 'i th diagonal =', i
+
+    #check first number of i th diagonal is odd or even
+    if i % 2 == 0:
+        #print 'upper if'
+        denominator = i + (num - total)
+        numerator = 1 - (num - total)
+    else:
+        #print 'else if'
+        denominator = 1 - (num - total)
+        numerator = i + (num - total)
+    print num, ' IS ', denominator, '/', numerator
+    return total
+
+
+count(input())
