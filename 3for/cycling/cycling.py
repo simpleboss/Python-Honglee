0a46310dd1261531a1435779c2dbde1ff6136065
gichan lee <gichan.lee@3mindev.club>
2017-11-14 14:34:41 +0900

    add number of empty bottle after drinking soda

---
+++ 3for/cycling/cycling.py
@@ -0,0 +1,22 @@
+def cycling(n, input_list):
+    range = 0
+    result = []
+    left = input_list[0]
+    for right in input_list:
+        if right - left > 0:
+            range += right - left
+        else:
+            if range != 0:
+                result.append(range)
+                range = 0
+        left = right
+        n += 1
+        result = sorted(result)
+        result = reversed(result)
+    return answer[0]
+
+
+n = int(raw_input())
+input_list = []
+input_list = [int(i) for i in raw_input().split(' ')]
+print cycling(n, input_list)
