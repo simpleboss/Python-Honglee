0b425c9173b4d70cb3f5f0ca2178b79aec7150f6
gichan lee <gichan.lee@3mindev.club>
2017-11-14 20:04:29 +0900

    remove all and code again

--- 3for/coins/coins.py
+++ 3for/coins/coins.py
@@ -1,14 +1,17 @@
 input_list = []
-i = 0
-while i >= 0:
-    input_list.append(int(raw_input()))
-    print input_list[-1]
-    if input_list[-1] == 0:
-        for j in range(input_list):
-            print 'input_list[j]', input_list[j]
-            if input_list[j] == 2:
-                print 'No Solution!'
-            else:
-                print input_list[j] -1
-        break
 
+
+def coins(input_list):
+    for i in input_list:
+        if i % 2 == 0:
+            print 'No Solution!'
+        else:
+            print i-1
+    return i
+
+
+input_list.append(int(raw_input()))
+while input_list[-1] != 0:
+    input_list.append(int(raw_input))
+
+coins(input_list)
