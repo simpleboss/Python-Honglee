17bb73fc79e95055af2d32db5e21d75a7f26b9cd
gichan lee <gichan.lee@3mindev.club>
2017-11-16 10:43:50 +0900

    multiple dataset error

--- 3for/coins/coins.py
+++ 3for/coins/coins.py
@@ -11,9 +11,10 @@ def coins(input_list):
             print i-1
     return i
 
-
+'''
 input_list.append(int(raw_input()))
 while input_list[-1] != 0:
     input_list.append(int(raw_input()))
-
+'''
+input_list = [int(i) for i in raw_input().split(' ')]
 coins(input_list)
