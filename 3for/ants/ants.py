b083f7926f3c23b32fbd07c4c987f9e52cd50099
gichan lee <gichan.lee@3mindev.club>
2017-11-16 17:47:12 +0900

    tested three times. and it works but when testing on 30staris, it doesn't work

--- 3for/ants/ants.py
+++ 3for/ants/ants.py
@@ -5,9 +5,17 @@ def ants(length, number_ants, ants_list):
         else:
             shortest_distance.append(length - i)
     shortest_distance.sort()
-    print shortest_distance[-1]
+    print shortest_distance[-1],
 
-    for i
+    for i in ants_list:
+        if i * 2 < length:
+            longest_distance.append(length - i)
+        else:
+            longest_distance.append(i)
+    longest_distance.sort()
+    print longest_distance[-1]
+    return shortest_distance, longest_distance
+
 
 input_list = []
 input_list = [int(i) for i in raw_input().split(' ')]
@@ -16,3 +24,5 @@ number_ants = input_list[1]
 ants_list = []
 ants_list = [int(i) for i in raw_input().split(' ')]
 shortest_distance = []
+longest_distance = []
+ants(length, number_ants, ants_list)
