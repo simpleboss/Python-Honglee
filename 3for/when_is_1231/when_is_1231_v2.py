83e71846d414d0b904001a5eb7859fdd77362eda
gichan lee <gichan.lee@3mindev.club>
2017-11-21 17:19:24 +0900

    def assert

--- 3for/when_is_1231/when_is_1231_v2.py
+++ 3for/when_is_1231/when_is_1231_v2.py
@@ -6,19 +6,23 @@ def when_is_first_day(n, input_days, sum):
     sum_d = 0
     for d in input_days:
         sum_d += d - input_days[0]
+    print 'n =', n
+    print 'sum_d =', sum_d
+    print 'sum = ', sum
     for day in range(1, 31):
+        print '     day =', day
+        print '          n * day + sum_d =', n * day + sum_d
         if n * day + sum_d == sum:
             first_day = day
             return first_day
-        else:
-            print 'No answer'
+            break
 
 
 def main():
     pass
 
 
-def test_for_when_is_first_day(n, input_days, sum):
+def test_for_when_is_first_day():
     print 'test1 for example'
     assert(when_is_first_day(3, [1, 2, 6], 18) == 4)
     print 'test2 for Nov'
@@ -27,3 +31,4 @@ def test_for_when_is_first_day(n, input_days, sum):
     assert (when_is_first_day(5, [0, 1, 4, 6, 7], 24+25+28+29+30) == 24)
     print 'Test Done'
 
+test_for_when_is_first_day()
