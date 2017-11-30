d4565db908979e45525fea30f725189643f2e5c2
gichan lee <gichan.lee@3mindev.club>
2017-11-30 17:50:08 +0900

    error

---
+++ 4MultipleLoop/ate/ate.py
@@ -0,0 +1,19 @@
+def input():
+    return int(raw_input())
+
+
+def ate(n):
+    answer = 0
+    for i in range(99):
+        for j in range(99):
+            if n - i - j == 0:
+                answer += 1
+    print answer
+
+
+def main():
+    n = input()
+    ate(n)
+
+
+main()
