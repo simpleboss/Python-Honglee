c87b94dcfa5effb279ace19488f7be8a19d475df
Gimun Lee <mrgomdev@3mindev.club>
2017-12-06 03:22:54 +0900

    Print chunks Repeat single lines Deal the last line

--- 4MultipleLoop/osciloscope_gimun/osciloscope.py
+++ 4MultipleLoop/osciloscope_gimun/osciloscope.py
@@ -1,20 +1,25 @@
+MAX_LINE_LENGTH = 60 # Default MAX_LINE_LENGTH
+
+
 def input():
-    return 5
+    return 4
 
 
-def print_first_chunk(amplitude):
-    def line(amplitude, y):
+def print_chunks(amplitude, line_length=MAX_LINE_LENGTH):
+    def line(amplitude, y, max_x):
         def build_span(len_total, len_left_blanks):
             """Build a span with blanks, star, and blanks.
                 len_total -- Total length of the span
                 len_left_blanks -- Length of the blanks before the star.
 
-                If len_left_blanks is less than 0, the span will be a 
+                If len_left_blanks is less than 0, the span has no left blank.
+                If len_left_blanks is larger than len_total, the span has no star and no right blank.
+                If len_left_blanks is equal to len_total, the span has no right blanks.
             """
             span = ''
 
             # line = ' ' * len_left_blanks + '*' * (len_total - (len_left_blanks + len_right_blanks)) + ' ' * len_right_blanks
-            for i in range(len_left_blanks):
+            for i in range(min(len_total, len_left_blanks)):
                 span += ' '
             if len_left_blanks < len_total and len_left_blanks >= 0:
                 span += '*'
@@ -23,23 +28,39 @@ def print_first_chunk(amplitude):
 
             return span
 
-        line = build_span(amplitude, y) + '*' + build_span((amplitude - 1), y + (amplitude - 1))
+        line = ''
+        while len(line) + (amplitude * 2) <= max_x:
+            line += build_span(amplitude, y) + '*' + build_span((amplitude - 1), y + (amplitude - 1))
+
+        # Last line
+        line += build_span(min(amplitude, max_x - len(line)), y)
+        rest = max_x - len(line)
+        if rest <= 0:
+            return line
+        
+        line += '*'
+        rest -= 1
+
+        if rest <= (amplitude - 1):
+            line += build_span(rest, y + (amplitude - 1))
 
         return line
 
     for y in range(amplitude, -(amplitude + 1), -1):
-        print '{}\t'.format(y) + line(amplitude, y)
+        print line(amplitude, y, line_length) + '|{:3}V'.format(y)
 
 
 def main():
     pass
 
 
-def test(n):
-    print_first_chunk(n)
+def test(n, line_length):
+    print_chunks(n, line_length)
 
 
 if __name__ == '__main__':
     # main()
-    n = input()
-    test(n)
+    for line_length in range(MAX_LINE_LENGTH):
+        n = input()
+        print 'line_length = {}'.format(line_length)
+        test(n, line_length)
