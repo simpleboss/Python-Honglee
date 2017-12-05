fb52f5bde5ae6ce203020751517ba9dfab9b526b
gichan lee <gichan.lee@3mindev.club>
2017-12-05 13:59:12 +0900

    input code

--- 4MultipleLoop/electrical_outlets/electrical_outlets.py
+++ 4MultipleLoop/electrical_outlets/electrical_outlets.py
@@ -1,1 +1,19 @@
-print 'test'
+def input():
+    n = raw_input()
+    input_list = []
+    for i in range(n):
+        input_list[i].append(int(i) for i in raw_input().split(' '))
+    return n, input_list
+
+
+def electrical_outlets(n):
+    pass
+
+
+def main():
+    n = input(raw_input)
+    input_list = input()
+    electrical_outlets(n, input_list)
+
+
+main()
