41ff200d5a89dd356190d30512ea1e024bdf0c11
Gimun Lee <mrgomdev@3mindev.club>
2017-12-06 02:07:02 +0900

    fix into pep8

--- 4MultipleLoop/osciloscope_gimun/osciloscope.py
+++ 4MultipleLoop/osciloscope_gimun/osciloscope.py
@@ -1,15 +1,19 @@
 def input():
     return 4
 
+
 def print_first_chunk(amplitude):
     for y in range(amplitude + 1):
         print y
 
+
 def main():
     pass
 
+
 def test(n):
     print_first_chunk(n)
+
 
 if __name__ == '__main__':
     # main()
