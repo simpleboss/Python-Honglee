8513d2e5b55032d68acd5bc0e7aa09aea02d3b54
Gimun Lee <mrgomdev@3mindev.club>
2017-12-06 02:45:48 +0900

    Print first chunk

--- 4MultipleLoop/osciloscope_gimun/osciloscope.py
+++ 4MultipleLoop/osciloscope_gimun/osciloscope.py
@@ -1,10 +1,34 @@
 def input():
-    return 4
+    return 5
 
 
 def print_first_chunk(amplitude):
-    for y in range(amplitude + 1):
-        print y
+    def line(amplitude, y):
+        def build_span(len_total, len_left_blanks):
+            """Build a span with blanks, star, and blanks.
+                len_total -- Total length of the span
+                len_left_blanks -- Length of the blanks before the star.
+
+                If len_left_blanks is less than 0, the span will be a 
+            """
+            span = ''
+
+            # line = ' ' * len_left_blanks + '*' * (len_total - (len_left_blanks + len_right_blanks)) + ' ' * len_right_blanks
+            for i in range(len_left_blanks):
+                span += ' '
+            if len_left_blanks < len_total and len_left_blanks >= 0:
+                span += '*'
+            for i in range(len_total - max(0, (len_left_blanks + 1))):
+                span += ' '
+
+            return span
+
+        line = build_span(amplitude, y) + '*' + build_span((amplitude - 1), y + (amplitude - 1))
+
+        return line
+
+    for y in range(amplitude, -(amplitude + 1), -1):
+        print '{}\t'.format(y) + line(amplitude, y)
 
 
 def main():
