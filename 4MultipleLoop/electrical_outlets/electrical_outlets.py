e20cf5a60b5a6d38064de2412a05d1f21f692990
gichan.lee <gichan.lee@3mindev.club>
2017-12-05 19:36:31 +0900

    modified input

--- 4MultipleLoop/electrical_outlets/electrical_outlets.py
+++ 4MultipleLoop/electrical_outlets/electrical_outlets.py
@@ -3,22 +3,29 @@ def input():
     input_list = []
     for i in range(n):
         i = int(i)
-        input_list.append([])
-        input_list[i].append([int(i) for i in raw_input().split(' ')])
+        input_list.append([int(i) for i in raw_input().split(' ')])
     input_list.append(n)
     return input_list
 
 
 def electrical_outlets(input_list):
     n = input_list[-1]
-    
-    pass
+    for i in range(n):
+        max = 0
+        j = 0
+        while j <= len(input_list[i]) - 1:
+            print 'input_list', input_list[i][j]
+            #max += int(input_list[i][j][0]) - 1
+            j += 1
+        max += 1
+        print max
 
 
 def main():
     input_list = input()
     electrical_outlets(input_list)
-    test(input_list)
+    #test(input_list)
+
 
 def test(input_list):
     print 'len(input_list)', len(input_list)
