1d6f250cd2d998ff221ede5bd8e1566a3fd62ebf
gichan.lee <gichan.lee@3mindev.club>
2017-12-07 11:50:11 +0900

    add def sugar, it works #done

--- 4MultipleLoop/sugar/sugar.py
+++ 4MultipleLoop/sugar/sugar.py
@@ -3,7 +3,23 @@ def input():
 
 
 def sugar(n):
-    pass
+    answer = []
+    if n % 3 == 0:
+        answer.append(n / 3)
+    if n % 5 == 0:
+        answer.append(n / 5)
+    for i in range(n // 3):
+        for j in range(n // 5):
+            if (3 * (i + 1)) + (5 * (j + 1)) == n:
+                answer.append(i + j + 2)
+    for i in range(n // 5):
+        for j in range(n // 3):
+            if (5 * (i + 1)) + (3 * (j + 1)) == n:
+                answer.append(i + j + 2)
+    if answer == []:
+        print '-1'
+    else:
+        print min(answer)
 
 
 def main():
