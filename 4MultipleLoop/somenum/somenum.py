21e2025177ea81cc6d056fddf19a81e91917ae42
gichan.lee <gichan.lee@3mindev.club>
2017-12-07 19:57:17 +0900

    #WIP

--- 4MultipleLoop/somenum/somenum.py
+++ 4MultipleLoop/somenum/somenum.py
@@ -2,7 +2,7 @@ def somenum():
     for i in range(1, 10001):
         count = 0
         k = 1
-        while k <= pow(i, 0.5):
+        while k <= pow(i, 0.5): 
             k += 1
         last_nb = k - 1
         j = 1
