629402554ca583f72063643286682c091a2f2f7b
gichan.lee <gichan.lee@3mindev.club>
2017-12-11 12:37:02 +0900

    #WIP. too complicated

--- 4MultipleLoop/multofn/multofn.py
+++ 4MultipleLoop/multofn/multofn.py
@@ -8,20 +8,31 @@ def multofn(n):
     for i in range(1111, 3333):
         if i % n == 0:
             nominate_list.append(i)
-    for j in nominate_list:
-        j_4 = j // 1000
-        j_3 = j // 100 - j_4 * 10
-        j_2 = j // 10 - j_4 * 100 - j_3 * 100
-        j_1 = j - j_4 * 1000 - j_3 * 100 - j_2 * 10
-        nb = 1
-        while nb < 4:
-            if j_4 != nb and j_3 != nb and j_2 != nb and j_1 != nb:
+    j_nb = []
+    for j in range(len(nominate_list)):
+        j_nb.append([])
+        j_nb[j].append(j // 1000)
+        j_nb[j].append(j // 100 - j_nb[j][0] * 10)
+        j_nb[j].append(j // 10 - j_nb[j][1] * 100 - j_nb[j][0] * 1000)
+        j_nb[j].append(j - j_nb[j][2] * 1000 - j_nb[j][1] * 100 - j_nb[j][0] * 1000)
+    for j in range(len(j_nb)):
+        for nb in j_nb[j]:
+            nb123 = 1
+            while nb123 < 4:
+                if j_nb[j][int(nb)] == nb123:
+                    j_nb[j].append('ok')
+                    break
+                nb123 += 1
+    answer_count = 0
+    for j in j_nb:
+        answer = -1
+        while answer > -5:
+            if j_nb[j][answer] != 'ok':
                 break
-            nb += 1
-            if nb == 4:
-                print 'j =', j
-                answer += 1
-    print answer
+            answer -= 1
+        if answer == -5:
+            answer_count += 1
+    print answer_count
 
 
 def main():
