d7c38060b1efdccf587ac94788ffd4dd7cbe31d4
Gimun Lee <mrgomdev@3mindev.club>
2017-12-11 16:16:19 +0900

    Solve in new way

--- 4MultipleLoop/multofn/multofn.py
+++ 4MultipleLoop/multofn/multofn.py
@@ -3,29 +3,33 @@ def input():
 
 
 def multofn(n):
-    nominate_list = []
-    answer = 0
-    for i in range(1111, 3333):
-        if i % n == 0:
-            nominate_list.append(i)
-    for j in nominate_list:
-        j_4 = j // 1000
-        j_3 = j // 100 - j_4 * 10
-        j_2 = j // 10 - j_4 * 100 - j_3 * 100
-        j_1 = j - j_4 * 1000 - j_3 * 100 - j_2 * 10
-        nb = 1
-        while nb < 4:
-            if j_4 != nb and j_3 != nb and j_2 != nb and j_1 != nb:
-                break
-            nb += 1
-            print nominate_list[j]
-        if nb == 4:            answer += 1
-    print answer
+    def digits_to_number(digits):
+        position_size = 1
+        number = 0
+        for i in range(len(digits)):
+            number += digits[-i -1] * position_size
+            position_size *= 10
+        return number
+    
+    digits = []
+    for i in range(4):
+        digits.append(0)
+    
+    multiples_3_count = 0
+    for digit_1000 in range(1, 4):
+        for digit_100 in range(1, 4):
+            for digit_10 in range(1, 4):
+                for digit_1 in range(1, 4):
+                    number = digits_to_number([digit_1000, digit_100, digit_10, digit_1]) 
+                    if number % n == 0:
+                        multiples_3_count += 1
+    return multiples_3_count
 
 
 def main():
-    n = input()
-    multofn(n)
+    # n = input()
+    n = 3
+    print multofn(n)
 
 
 main()
