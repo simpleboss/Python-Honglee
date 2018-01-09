98a1b7d05d801c1ad8391fac81d9340c0a8d470e
gichan.lee <gichan.lee@3mindev.club>
2018-01-09 21:52:28 +0900

    add everything

--- 4MultipleLoop/sclock/sclock.py
+++ 4MultipleLoop/sclock/sclock.py
@@ -1,29 +1,25 @@
 def input():
-    return raw_input()
+    return int(raw_input())
 
 
-def sclock(n):
-    for i in range(n // 2):
-        a_line = ''
-        for j in range(i):
-            a_line += ' '
-        for j in range(n - 1 - 2 * i):
-            a_line += '*'
-        a_line += '$'
-        print a_line
-    for i in range(n // 2 + 1):
-        a_line = ''
-        for j in range(n // 2 - i):
-            a_line += ' '
-        a_line += '$'
-        for j in range(2 * i):
-            a_line += '*'
-        print a_line
+def main(n):
+    for space in range(0, n // 2 + 1):
+        line = ''
+        for i in range(space):
+            line += ' '
+        for i in range(n - 1 - (space * 2)):
+            line += '*'
+        line += '$'
+        print line
+    for space in range(n // 2 - 1, -1, -1):
+        line = ''
+        for i in range(space):
+            line += ' '
+        line += '$'
+        for i in range(0, 2 * (n // 2 - space), 1):
+            line += '*'
+        print line
 
 
-def main():
-    n = int(input())
-    sclock(n)
-
-
-main()
+n = input()
+main(n)
