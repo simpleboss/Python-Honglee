8eced3a905c94adc63fec0407acccf957c5255e5
Gimun Lee <mrgomdev@3mindev.club>
2018-01-10 22:59:18 +0900

    add print_triangle

--- 4MultipleLoop/sclock/sclock.py
+++ 4MultipleLoop/sclock/sclock.py
@@ -3,22 +3,44 @@ def input():
 
 
 def main(n):
-    for space in range(0, n // 2 + 1):
-        line = ''
-        for i in range(space):
-            line += ' '
-        for i in range(n - 1 - (space * 2)):
-            line += '*'
-        line += '$'
-        print line
-    for space in range(n // 2 - 1, -1, -1):
-        line = ''
-        for i in range(space):
-            line += ' '
-        line += '$'
-        for i in range(0, 2 * (n // 2 - space), 1):
-            line += '*'
-        print line
+    def print_triangle(is_top):
+        if is_top:
+            space_range = range(0, n // 2 + 1)
+        else:
+            space_range = range(n // 2 - 1, -1, -1)
+
+        for space in space_range:
+            count_asterisks = n - 2 * space - 1
+
+            line = ''
+            for i in range(space):
+                line += ' '
+            
+            if not is_top:
+                line += '$'
+            for i in range(count_asterisks):
+                line += '*'
+            if is_top:
+                line += '$'
+            print line
+    print_triangle(True)
+    print_triangle(False)
+    # for space in range(0, n // 2 + 1):
+    #     line = ''
+    #     for i in range(space):
+    #         line += ' '
+    #     for i in range(n - 1 - (space * 2)):
+    #         line += '*'
+    #     line += '$'
+    #     print line
+    # for space in range(n // 2 - 1, -1, -1):
+    #     line = ''
+    #     for i in range(space):
+    #         line += ' '
+    #     line += '$'
+    #     for i in range(0, 2 * (n // 2 - space), 1):
+    #         line += '*'
+    #     print line
 
 
 n = input()
