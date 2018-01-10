ba26c49ed53cac01855e7ff2cc81a6196df20621
Gimun Lee <mrgomdev@3mindev.club>
2018-01-10 22:59:42 +0900

    remove previous codes

--- 4MultipleLoop/sclock/sclock.py
+++ 4MultipleLoop/sclock/sclock.py
@@ -25,22 +25,6 @@ def main(n):
             print line
     print_triangle(True)
     print_triangle(False)
-    # for space in range(0, n // 2 + 1):
-    #     line = ''
-    #     for i in range(space):
-    #         line += ' '
-    #     for i in range(n - 1 - (space * 2)):
-    #         line += '*'
-    #     line += '$'
-    #     print line
-    # for space in range(n // 2 - 1, -1, -1):
-    #     line = ''
-    #     for i in range(space):
-    #         line += ' '
-    #     line += '$'
-    #     for i in range(0, 2 * (n // 2 - space), 1):
-    #         line += '*'
-    #     print line
 
 
 n = input()
