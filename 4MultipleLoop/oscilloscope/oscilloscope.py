aab077cfd0b43871677fd31658c57c7b13ce4838
gichan.lee <gichan.lee@3mindev.club>
2018-01-11 15:21:08 +0900

    add loop code

--- 4MultipleLoop/oscilloscope/oscilloscope.py
+++ 4MultipleLoop/oscilloscope/oscilloscope.py
@@ -6,28 +6,39 @@ def main(input_v):
     def make_row(input_v):
         row = []
         for i in range(2 * input_v + 1):
-            row.append([])
+            row.append([''])
+        return row
+
+    def line_star(row, input_v):
+        for i in range(len(row)):
+            row[i] += '*'
+
+    def space_after_line_star(row, input_v):
+        for i in range(1, len(row), 1):
+            for j in range(i - 1):
+                row[i] += ' '
         return row
 
     def stair_star(row, input_v):
-        for i in range(len(row)):
-            for j in range(i):
-                row[i] += ' '
+        for i in range(1, len(row) - 1):
             row[i] += '*'
         return row
 
-    def space_between_stars(row, input_v):
+    def space_after_stair_star(row, input_v):
         for i in range(len(row)):
-            for j in range(2 * input_v - i):
+            for j in range(2 * input_v - i -1):
                 row[i] += ' '
-            row[i] += '*'
         return row
 
     row = make_row(input_v)
-    stair_star(row, input_v)
-    space_between_stars(row, input_v)
+    for i in range(2):
+        line_star(row, input_v)
+        space_after_line_star(row, input_v)
+        stair_star(row, input_v)
+        space_after_stair_star(row, input_v)
 
     for i in range(len(row) - 1, -1, -1):
+        #print i
         print("".join(str(x) for x in row[i]))
 
 
