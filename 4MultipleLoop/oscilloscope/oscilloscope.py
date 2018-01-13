9be3587c3bfd2f085a6b0c70f25459fc9cb699a0
Gimun Lee <mrgomdev@3mindev.club>
2018-01-13 16:02:29 +0900

    replace row to rows

--- 4MultipleLoop/oscilloscope/oscilloscope.py
+++ 4MultipleLoop/oscilloscope/oscilloscope.py
@@ -3,48 +3,48 @@ def input():
 
 
 def main(input_v):
-    def make_row(input_v):
-        row = []
+    def make_rows(input_v):
+        rows = []
         for i in range(2 * input_v + 1):
-            row.append([''])
-        return row
+            rows.append([''])
+        return rows
 
-    def line_star(row, input_v):
-        for i in range(len(row)):
-            row[i] += '*'
+    def line_star(rows, input_v):
+        for i in range(len(rows)):
+            rows[i] += '*'
 
-    def space_after_line_star(row, input_v):
-        for i in range(1, len(row), 1):
+    def space_after_line_star(rows, input_v):
+        for i in range(1, len(rows), 1):
             for j in range(i - 1):
-                row[i] += ' '
-        return row
+                rows[i] += ' '
+        return rows
 
-    def stair_star(row, input_v):
-        for i in range(1, len(row) - 1):
-            row[i] += '*'
-        return row
+    def stair_star(rows, input_v):
+        for i in range(1, len(rows) - 1):
+            rows[i] += '*'
+        return rows
 
-    def space_after_stair_star(row, input_v):
-        for i in range(len(row)):
+    def space_after_stair_star(rows, input_v):
+        for i in range(len(rows)):
             for j in range(2 * input_v - i - 1):
-                row[i] += ' '
-        return row
+                rows[i] += ' '
+        return rows
 
-    row = make_row(input_v)
+    rows = make_rows(input_v)
     current_column = 0
     while current_column < 61 + input_v:
-        line_star(row, input_v)
-        space_after_line_star(row, input_v)
-        stair_star(row, input_v)
-        space_after_stair_star(row, input_v)
-        current_column = len(row[1])
+        line_star(rows, input_v)
+        space_after_line_star(rows, input_v)
+        stair_star(rows, input_v)
+        space_after_stair_star(rows, input_v)
+        current_column = len(rows[1])
 
     result = []
     if input_v >= 10:
         high_limit = 20 + input_v - 10
         low_limit = input_v - 10
         for i in range(low_limit, high_limit + 1):
-            result.append(row[i])
+            result.append(rows[i])
     else:
         # high_limit = input_v * 2
         # low_limit = -1
@@ -52,8 +52,8 @@ def main(input_v):
             result.append('')
             for j in range(2 * input_v + 60):
                 result[-1] += ' '
-        for i in range(len(row)):
-            result.append(row[i])
+        for i in range(len(rows)):
+            result.append(rows[i])
         for i in range(10 - input_v):
             result.append('')
             for j in range(2 * input_v + 60):
