d6a6c9603557598ae4583b7e24e48481a4d15691
gichan.lee <gichan.lee@3mindev.club>
2018-01-13 11:09:30 +0900

    modify print result code to remove space between two print

--- 4MultipleLoop/oscilloscope/oscilloscope.py
+++ 4MultipleLoop/oscilloscope/oscilloscope.py
@@ -26,13 +26,13 @@ def main(input_v):
 
     def space_after_stair_star(row, input_v):
         for i in range(len(row)):
-            for j in range(2 * input_v - i -1):
+            for j in range(2 * input_v - i - 1):
                 row[i] += ' '
         return row
 
     row = make_row(input_v)
     current_column = 0
-    while current_column < 60 + input_v:
+    while current_column < 61 + input_v:
         line_star(row, input_v)
         space_after_line_star(row, input_v)
         stair_star(row, input_v)
@@ -46,14 +46,21 @@ def main(input_v):
         for i in range(low_limit, high_limit + 1):
             result.append(row[i])
     else:
-        high_limit = input_v * 2
-        low_limit = -1
-    print len(result)
+        # high_limit = input_v * 2
+        # low_limit = -1
+        for i in range(10 - input_v):
+            result.append('')
+            for j in range(2 * input_v + 60):
+                result[-1] += ' '
+        for i in range(len(row)):
+            result.append(row[i])
+        for i in range(10 - input_v):
+            result.append('')
+            for j in range(2 * input_v + 60):
+                result[-1] += ' '
+
     for i in range(20, -1, -1):
-        #print i
-        print("".join(str(x) for x in result[i][input_v + 1:61 + input_v])),
-        print '| ', i, 'V'
+        print("".join(str(x) for x in result[i][input_v + 1:61 + input_v]) + '|{:3}V'.format(i-10))
 
-
-input_v = 10
+input_v = input()
 main(input_v)
